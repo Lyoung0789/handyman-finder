c9eb37526641d61bfddf46f84fdd52ae106fb756
Lyoung0789 <Lyoung070789@gmail.com>
2020-05-04 17:45:15 -0400

    added show routes and vieww

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,20 @@ class UsersController < ApplicationController
     end 
 
     def create
-        byebug
+        if (user = User.create(user_params))
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            render 'new'
+        end
     end 
     
+    def show 
+        
+        @user = User.find_by(id: params[:id])
+
+    end 
+
     private 
     def user_params
         params.require(:user).permit(:first_name, :last_name, :email, :phone_number, :username, :password)
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get '/signup', to: "users#new"
   post '/signup', to: "users#create"
+  get '/users/:id', to: "users#show" 
   resources :appointments
   resources :technicians
-  resources :users
+  # resources :users
   get '/', to: "welcome#hello" 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
