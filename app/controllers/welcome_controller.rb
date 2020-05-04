9025f04a1434eb257b2f8398765cff1dd32247fe
Lyoung0789 <Lyoung070789@gmail.com>
2020-05-04 19:54:45 -0400

    going to / will redirect user to their show page if logged in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,9 @@
 class WelcomeController < ApplicationController
     skip_before_action :verified_user, only: [:hello]
     def hello
+        # byebug
+        if current_user 
+            redirect_to user_path(current_user)
+        end 
     end 
 end
