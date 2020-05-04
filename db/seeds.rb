3576f8f313d24d3fb1551a1c4e07da011ff069e0
Lyoung0789 <Lyoung070789@gmail.com>
2020-05-04 14:57:13 -0400

    added appointment seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,4 @@
 
 user_1 = User.create(first_name: "Jack", last_name: "Jones", username: "JJ", email:"jackjones@test.com", password: "jack", phone_number: "555-555-5555")
 technician = Technician.create(name: "Joe_tech", phone_number:"111-111-1111", rating: 5, category: "Appliance")
+app = Appointment.create(date: DateTime.current, issue:"Fridge is broken", technician_id: 1, user_id: 1)
