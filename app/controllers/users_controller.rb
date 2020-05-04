76d3343bfdc081e164af4ef572a2b79cf2d7a854
Lyoung0789 <Lyoung070789@gmail.com>
2020-05-04 16:56:13 -0400

    added form to sign up and created user_params method to create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,16 @@
 class UsersController < ApplicationController
+
+    def new 
+        # binding.pry
+        @user = User.new
+    end 
+
+    def create
+        byebug
+    end 
     
+    private 
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :phone_number, :username, :password)
+    end 
 end
