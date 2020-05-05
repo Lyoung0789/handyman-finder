8a2bba612da08ecedb660b9d1f548f73e1a03896
Lyoung0789 <Lyoung070789@gmail.com>
2020-05-05 18:32:08 -0400

    added technician checkbox at signup page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     before_action :verified_user
     helper_method :current_user
+    helper_method :logged_in?
 
     private 
     def verified_user
@@ -15,4 +16,9 @@ class ApplicationController < ActionController::Base
       def current_user
         @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
       end
+
+      def logged_in?
+        !!session[:user_id]
+      end 
+
 end
