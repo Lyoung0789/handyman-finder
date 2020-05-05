c3b4ff2d10c2c95f84e31cecd711fa74c915bc79
Lyoung0789 <Lyoung070789@gmail.com>
2020-05-05 13:14:45 -0400

    added appointment controller new and create methods

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,6 +2,7 @@ class AppointmentsController < ApplicationController
     def new 
         # byebug
         if params[:technician_id] && tech = Technician.find_by(id: params[:technician_id])
+            # byebug
             @appointment = tech.appointments.build
         else 
             @appointment = Appointment.new
@@ -9,8 +10,21 @@ class AppointmentsController < ApplicationController
     end 
 
     def create 
+        # byebug
+        @appointment = current_user.appointments.build(appointment_params)
+
+        if @appointment.save
+            redirect_to user_path(current_user)
+        else 
+            render :new 
+        end 
 
     end 
 
 
+    private 
+
+    def appointment_params
+        params.require(:appointment).permit(:date, :issue, :technician_id)
+    end 
 end
