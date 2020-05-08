c8142304bbec6b209e909607e3be3a46514b5675
Lyoung0789 <Lyoung070789@gmail.com>
2020-05-07 21:56:19 -0400

    added validation for user/show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,10 @@ class UsersController < ApplicationController
     skip_before_action :verified_user, only: [:new, :create]
 
     def new 
-        # binding.pry
         @user = User.new
     end 
 
     def create
-        # binding.pry
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
@@ -18,19 +16,21 @@ class UsersController < ApplicationController
     end 
     
     def show 
-        if current_user.id != params[:id].to_i
-            redirect_to user_path(current_user)
-        end 
-        @user = User.find_by(id: params[:id])
-        # byebug
-    end 
-
-    def edit 
-        # binding.pry
         if correct_user?
             @user = User.find_by(id: params[:id])
         else 
-            flash[:error] = "You do not have access to that page"
+            flash[:error] = "You do not have access to that page."
+            redirect_to user_path(current_user)
+        end 
+        
+    end 
+
+    def edit 
+        
+        if correct_user?
+            @user = User.find_by(id: params[:id])
+        else 
+            flash[:error] = "You do not have access to that page."
             redirect_to user_path(current_user)
         end 
 
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
     end 
 
     def update 
-        # binding.pry
+        
         @user = User.find_by(id: params[:id])
 
         if @user.update(user_params)
